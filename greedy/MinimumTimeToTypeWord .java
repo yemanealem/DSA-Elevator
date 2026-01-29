74aa19d297eea01190098a4c3691c19decedbc5e
yemanealem <yemanea223@gmail.com>
2026-01-29 23:49:59 +0100

    Refactored Minimum Time to Type Word Using Special Typewriter

--- greedy/MinimumTimeToTypeWord .java
+++ greedy/MinimumTimeToTypeWord .java
@@ -1,5 +1,27 @@
-public class MinimumTimeToTypeWord  {
-    int time = 0;
+public class MinimumTimeToTypeWord {
+
+    /*
+     * LeetCode 1974: Minimum Time to Type Word Using Special Typewriter
+     *
+     * There is a special typewriter with lowercase English letters 'a' to 'z'
+     * arranged in a circular manner.
+     *
+     * - The typewriter initially points to 'a'
+     * - Typing the current character takes 1 second
+     * - Moving the pointer one step clockwise or counterclockwise takes 1 second
+     *
+     * You can move in either direction.
+     *
+     * Return the minimum time required to type the given word.
+     *
+     * Greedy Insight:
+     * - At each character, choose the shorter rotation (clockwise or counterclockwise)
+     * - Add 1 second for typing the character
+     * - Repeat for all characters
+     */
+
+    public static int minTimeToType(String word) {
+        int time = 0;
         char current = 'a'; // initial pointer position
 
         for (char c : word.toCharArray()) {
@@ -16,4 +38,15 @@ public class MinimumTimeToTypeWord  {
         }
 
         return time;
+    }
+
+    // ---------------- MAIN METHOD ----------------
+    public static void main(String[] args) {
+
+        String word = "abc";
+
+        int result = minTimeToType(word);
+
+        System.out.println("Minimum time to type the word: " + result);
+    }
 }
