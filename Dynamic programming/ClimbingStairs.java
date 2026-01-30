35cda4fca519332c1d6b0075b1bbd650dedcaf97
yemanealem <yemanea223@gmail.com>
2026-01-30 20:37:30 +0100

    climbing stair

---
+++ Dynamic programming/ClimbingStairs.java
@@ -0,0 +1,23 @@
+public class ClimbingStairs {
+
+   
+    public static int climbStairs(int n) {
+        if (n <= 1) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(2)); // 2
+        System.out.println(climbStairs(3)); // 3
+        System.out.println(climbStairs(5)); // 8
+    }
+}
