d9c0bb1f8266c3b238737d16f21be056d34f6911
yemanealem <yemanea223@gmail.com>
2026-02-03 17:39:34 +0100

    refactored power of two

--- bitmanipulation/IsPowerOfTwo.java
+++ bitmanipulation/IsPowerOfTwo.java
@@ -1,10 +1,26 @@
-class Solution {
-    public boolean isPowerOfTwo(int n) {
-        if (n <= 0) return false;
+// LeetCode 231: Power of Two
+// Problem: Given an integer n, return true if it is a power of two, else false.
+// Explanation: 
+// A power of two has exactly one '1' bit in binary.
+// Using bit manipulation: n & (n - 1) == 0 checks this condition in O(1) time.
+// Edge cases: n <= 0 returns false.
 
-        while (n % 2 == 0) {
-            n /= 2;
+public class IsPowerOfTwo {
+
+    static class Solution {
+        public boolean isPowerOfTwo(int n) {
+            // Returns true if n is a power of two
+            return n > 0 && (n & (n - 1)) == 0;
         }
-        return n == 1;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] testCases = {1, 2, 3, 4, 16, 18, 0, -8};
+
+        for (int n : testCases) {
+            System.out.println(n + " is power of two? " + sol.isPowerOfTwo(n));
+        }
     }
 }
