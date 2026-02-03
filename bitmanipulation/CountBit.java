6a620ef41d0c2f4aa437df608c06029c3d4ed557
yemanealem <yemanea223@gmail.com>
2026-02-03 17:53:55 +0100

    refactoring count bit

--- bitmanipulation/CountBit.java
+++ bitmanipulation/CountBit.java
@@ -1,12 +1,19 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] countBits(int n) {
         int[] bits = new int[n + 1];
-        bits[0] = 0; // 0 has 0 bits
-
         for (int i = 1; i <= n; i++) {
-            bits[i] = bits[i & (i - 1)] + 1; // remove lowest set bit +1
+            bits[i] = bits[i & (i - 1)] + 1;
         }
-
         return bits;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n = 10;
+        int[] result = sol.countBits(n);
+        System.out.println(Arrays.toString(result)); 
+        // Output: [0,1,1,2,1,2,2,3,1,2,2]
     }
 }
