4d837c5cee582bfa69e261c25089124d08ebd851
yemanealem <yemanea223@gmail.com>
2026-02-05 16:29:40 +0100

    container with most water

---
+++ twopointer/MaxArea.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int h = Math.min(height[left], height[right]);
+            int area = h * width;
+
+            maxArea = Math.max(maxArea, area);
+
+            // Move the pointer with smaller height
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
