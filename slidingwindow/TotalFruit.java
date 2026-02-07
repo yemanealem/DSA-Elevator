a2d15471d25dac3b242d18dd1ba1713e8c52f9d9
yemanealem <yemanea223@gmail.com>
2026-02-07 17:46:56 +0100

    refactored and optimized total fruit

--- slidingwindow/TotalFruit.java
+++ slidingwindow/TotalFruit.java
@@ -1,30 +1,47 @@
-class Solution {
-    public int totalFruit(int[] fruits) {
-        if (fruits.length == 0) return 0;
+public class TotalFruit {
 
-        int maxFruits = 0;
+    // Optimized sliding window solution
+    public int totalFruit(int[] fruits) {
+        int ans = 0;
         int lastFruit = -1, secondLastFruit = -1;
-        int lastFruitCount = 0;
+        int lastFruitCount = 0; // consecutive count of lastFruit
         int currentMax = 0;
 
         for (int fruit : fruits) {
             if (fruit == lastFruit || fruit == secondLastFruit) {
-                currentMax += 1; 
+                // extend the window
+                currentMax++;
             } else {
-                currentMax = lastFruitCount + 1; 
+                // reset window: last contiguous segment + new fruit
+                currentMax = lastFruitCount + 1;
             }
 
+            // update lastFruitCount and track last/secondLast fruit
             if (fruit == lastFruit) {
-                lastFruitCount += 1;
+                lastFruitCount++;
             } else {
-                lastFruitCount = 1; 
+                lastFruitCount = 1;
                 secondLastFruit = lastFruit;
                 lastFruit = fruit;
             }
 
-            maxFruits = Math.max(maxFruits, currentMax);
+            ans = Math.max(ans, currentMax);
         }
 
-        return maxFruits; 
+        return ans;
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        TotalFruit solution = new TotalFruit();
+
+        int[] fruits1 = {1, 2, 1};
+        System.out.println("Max fruits collected: " + solution.totalFruit(fruits1)); // 3
+
+        int[] fruits2 = {0, 1, 2, 2};
+        System.out.println("Max fruits collected: " + solution.totalFruit(fruits2)); // 3
+
+        int[] fruits3 = {1, 2, 3, 2, 2};
+        System.out.println("Max fruits collected: " + solution.totalFruit(fruits3)); // 4
     }
 }
