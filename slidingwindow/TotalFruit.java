2fe612f75f20f5e32e61587ce86ec270435087bd
yemanealem <yemanea223@gmail.com>
2026-02-07 17:48:01 +0100

    refactored and optimized total fruit

--- slidingwindow/TotalFruit.java
+++ slidingwindow/TotalFruit.java
@@ -1,18 +1,39 @@
+/*
+LeetCode 904 – Fruit Into Baskets
+
+Question:
+You are given an integer array fruits where fruits[i] represents the type of fruit on the i-th tree.
+You have two baskets, and each basket can only hold one type of fruit, but you can pick any amount of that fruit.
+You want to pick as many fruits as possible in one contiguous segment of trees.
+Return the maximum number of fruits you can collect.
+
+Example:
+Input: [0,1,2,2]
+Output: 3
+Explanation: We can pick [1,2,2] -> max 3 fruits.
+
+Approach:
+- Use a sliding window keeping track of the last two fruit types.
+- Track lastFruitCount = consecutive count of lastFruit.
+- If the new fruit is one of the last two, extend window.
+- Else, reset window to lastFruitCount + 1 (new fruit starts a new window).
+- Update max each step.
+*/
+
 public class TotalFruit {
 
-    // Optimized sliding window solution
     public int totalFruit(int[] fruits) {
         int ans = 0;
-        int lastFruit = -1, secondLastFruit = -1;
+        int lastFruit = -1, secondLastFruit = -1; // last two fruit types
         int lastFruitCount = 0; // consecutive count of lastFruit
-        int currentMax = 0;
+        int currentMax = 0; // current window length
 
         for (int fruit : fruits) {
             if (fruit == lastFruit || fruit == secondLastFruit) {
-                // extend the window
+                // extend the window if fruit is one of the last two types
                 currentMax++;
             } else {
-                // reset window: last contiguous segment + new fruit
+                // new fruit type, reset window: last contiguous segment + new fruit
                 currentMax = lastFruitCount + 1;
             }
 
@@ -31,17 +52,34 @@ public class TotalFruit {
         return ans;
     }
 
-    // Main method for testing
     public static void main(String[] args) {
         TotalFruit solution = new TotalFruit();
 
-        int[] fruits1 = {1, 2, 1};
-        System.out.println("Max fruits collected: " + solution.totalFruit(fruits1)); // 3
+        int[] fruits = {0,1,2,2};
 
-        int[] fruits2 = {0, 1, 2, 2};
-        System.out.println("Max fruits collected: " + solution.totalFruit(fruits2)); // 3
+        /*
+        Trace for [0,1,2,2]:
+        Index  Fruit  Last  SecondLast  LastCount  CurrentMax  Max
+        0      0      0     -1          1          1           1
+        1      1      1      0          1          2           2
+        2      2      2      1          1          2           2
+        3      2      2      1          2          3           3
+        Answer = 3
+        */
 
-        int[] fruits3 = {1, 2, 3, 2, 2};
-        System.out.println("Max fruits collected: " + solution.totalFruit(fruits3)); // 4
+        System.out.println("Max fruits collected: " + solution.totalFruit(fruits)); // Output: 3
+
+        int[] fruits2 = {1,2,3,2,2};
+        /*
+        Trace for [1,2,3,2,2]:
+        Index  Fruit  Last  SecondLast  LastCount  CurrentMax  Max
+        0      1      1     -1          1          1           1
+        1      2      2      1          1          2           2
+        2      3      3      2          1          2           2
+        3      2      2      3          1          2           2
+        4      2      2      3          2          3           3
+        Answer = 4
+        */
+        System.out.println("Max fruits collected: " + solution.totalFruit(fruits2)); // Output: 4
     }
 }
