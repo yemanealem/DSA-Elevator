5951827467e1c563b3b13a4d41b4b3bfdbe3664b
yemanealem <yemanea223@gmail.com>
2026-02-09 22:22:25 +0100

    refactored permutation

--- backtracking/Parmute.java
+++ backtracking/Parmute.java
@@ -2,31 +2,31 @@ class Solution {
 
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        boolean[] used = new boolean[nums.length];
-        backtrack(nums, new ArrayList<>(), used, result);
+        backtrack(nums, 0, result);
         return result;
     }
 
-    private void backtrack(int[] nums, List<Integer> current,
-                           boolean[] used, List<List<Integer>> result) {
+    private void backtrack(int[] nums, int index, List<List<Integer>> result) {
 
-        // base case
-        if (current.size() == nums.length) {
-            result.add(new ArrayList<>(current)); // make a copy
+        if (index == nums.length) {
+            List<Integer> permutation = new ArrayList<>();
+            for (int num : nums) {
+                permutation.add(num);
+            }
+            result.add(permutation);
             return;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (used[i]) continue;   // skip used numbers
-
-            used[i] = true;
-            current.add(nums[i]);
-
-            backtrack(nums, current, used, result);
-
-            // backtrack
-            current.remove(current.size() - 1);
-            used[i] = false;
+        for (int i = index; i < nums.length; i++) {
+            swap(nums, index, i);        // choose
+            backtrack(nums, index + 1, result); // explore
+            swap(nums, index, i);        // un-choose
         }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
