e3fa1b566576e8f3207d1310071a52bb4eb1d97f
yemanealem <yemanea223@gmail.com>
2026-02-11 22:27:58 +0100

    refactored add two number

--- recursion/AddTwoNumber.java
+++ recursion/AddTwoNumber.java
@@ -1,20 +1,20 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+class ListNode {
+    int val;
+    ListNode next;
 
-class Solution {
+    ListNode(int val) {
+        this.val = val;
+    }
+}
 
+public class AddTwoNumber {
+
+    // Main add method (calls recursive helper)
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         return add(l1, l2, 0);
     }
 
+    // Recursive helper
     private ListNode add(ListNode l1, ListNode l2, int carry) {
 
         // Base case
@@ -41,5 +41,45 @@ class Solution {
         node.next = add(l1, l2, sum / 10);
 
         return node;
+    }
+
+    // Utility method to print linked list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+            if (head.next != null) {
+                System.out.print(" -> ");
+            }
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    // MAIN METHOD
+    public static void main(String[] args) {
+
+        AddTwoNumber solution = new AddTwoNumber();
+
+        // Create first number: 2 -> 4 -> 3
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        // Create second number: 5 -> 6 -> 4
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        System.out.println("First Number:");
+        printList(l1);
+
+        System.out.println("Second Number:");
+        printList(l2);
+
+        // Add numbers
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        System.out.println("Result:");
+        printList(result);
     }
 }
