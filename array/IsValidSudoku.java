3fee859ff783b83baa42f8aa92ce58e5e708d113
yemanealem <yemanea223@gmail.com>
2026-02-17 17:52:17 +0100

    refactored is valid sundoku

--- array/IsValidSudoku.java
+++ array/IsValidSudoku.java
@@ -1,14 +1,8 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-         HashSet<Character>[] rows = new HashSet[9];
-        HashSet<Character>[] cols = new HashSet[9];
-        HashSet<Character>[] boxes = new HashSet[9];
-        
-        for (int i = 0; i < 9; i++) {
-            rows[i] = new HashSet<>();
-            cols[i] = new HashSet<>();
-            boxes[i] = new HashSet<>();
-        }
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
         
         for (int r = 0; r < 9; r++) {
             for (int c = 0; c < 9; c++) {
@@ -19,17 +13,16 @@ class Solution {
                     continue;
                 }
                 
+                int num = current - '1';  // convert '1'-'9' to 0-8
                 int boxIndex = (r / 3) * 3 + (c / 3);
                 
-                if (rows[r].contains(current) ||
-                    cols[c].contains(current) ||
-                    boxes[boxIndex].contains(current)) {
+                if (rows[r][num] || cols[c][num] || boxes[boxIndex][num]) {
                     return false;
                 }
                 
-                rows[r].add(current);
-                cols[c].add(current);
-                boxes[boxIndex].add(current);
+                rows[r][num] = true;
+                cols[c][num] = true;
+                boxes[boxIndex][num] = true;
             }
         }
         
