6a773e49701cc61b51cc2ee0dff5e6d7ed6d9460
yemanealem <yemanea223@gmail.com>
2026-02-19 18:45:10 +0100

    refactored longest increasing path

--- Breadth First Search (BFS)/LongestIncreasingPath.java
+++ Breadth First Search (BFS)/LongestIncreasingPath.java
@@ -1,38 +1,57 @@
-class Solution {
-    private int[][] dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}}; // up, down, left, right
-    
+public class LongestIncreasingPath {
+
+    private static final int[][] DIRS = {{-1,0},{1,0},{0,-1},{0,1}}; // up, down, left, right
+
+    // DFS + Memoization method
     public int longestIncreasingPath(int[][] matrix) {
         if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
-        
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[][] memo = new int[m][n]; // stores longest path starting at (i,j)
-        
+
+        int m = matrix.length, n = matrix[0].length;
+        int[][] memo = new int[m][n]; // memo[i][j] = longest path starting at (i,j)
         int maxPath = 0;
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 maxPath = Math.max(maxPath, dfs(matrix, i, j, memo));
             }
         }
+
         return maxPath;
     }
-    
-    private int dfs(int[][] matrix, int i, int j, int[][] memo) {
-        if(memo[i][j] != 0) return memo[i][j]; // already computed
-        
-        int maxLen = 1; // minimum path length starting at this cell
-        for(int[] dir : dirs) {
-            int ni = i + dir[0];
-            int nj = j + dir[1];
-            // move to neighbor only if value is increasing
-            if(ni >= 0 && ni < matrix.length && nj >= 0 && nj < matrix[0].length 
-               && matrix[ni][nj] > matrix[i][j]) {
-                int len = 1 + dfs(matrix, ni, nj, memo);
-                maxLen = Math.max(maxLen, len);
+
+    private int dfs(int[][] matrix, int x, int y, int[][] memo){
+        if(memo[x][y] != 0) return memo[x][y]; // already computed
+
+        int maxLen = 1; // at least the cell itself
+        for(int[] dir : DIRS){
+            int nx = x + dir[0], ny = y + dir[1];
+            if(nx >= 0 && nx < matrix.length && ny >= 0 && ny < matrix[0].length
+               && matrix[nx][ny] > matrix[x][y]){
+                maxLen = Math.max(maxLen, 1 + dfs(matrix, nx, ny, memo));
             }
         }
-        
-        memo[i][j] = maxLen; // memoize
+
+        memo[x][y] = maxLen;
         return maxLen;
+    }
+
+    // Main method to test
+    public static void main(String[] args) {
+        LongestIncreasingPath lip = new LongestIncreasingPath();
+
+        int[][] matrix1 = {
+            {9,9,4},
+            {6,6,8},
+            {2,1,1}
+        };
+
+        int[][] matrix2 = {
+            {3,4,5},
+            {3,2,6},
+            {2,2,1}
+        };
+
+        System.out.println("Longest Increasing Path (matrix1): " + lip.longestIncreasingPath(matrix1)); // Output: 4
+        System.out.println("Longest Increasing Path (matrix2): " + lip.longestIncreasingPath(matrix2)); // Output: 4
     }
 }
