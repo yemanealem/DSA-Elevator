4a7abf305372366291d5947405ab8763b33ea268
yemanealem <yemanea223@gmail.com>
2026-02-21 23:38:08 +0100

    binary-tree-inorder-traversal

---
+++ Depth-First Search/InorderTraversalIterative.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class InorderTraversalIterative {
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            result.add(current.val);
+
+            current = current.right;
+        }
+
+        return result;
+    }
+}
