0486a6f44a8f40c9a491afe6a2b6e05c929c72e8
yemanealem <yemanea223@gmail.com>
2026-02-23 18:27:04 +0100

    find if path exit in graph

--- graphtheory/FindIfPathExistsInGraph.java
+++ graphtheory/FindIfPathExistsInGraph.java
@@ -1,20 +1,17 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>> graph = new ArrayList<>();
-
+        List<List<Integer>> graph = new ArrayList<>(n);
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
 
         for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
         }
 
         boolean[] visited = new boolean[n];
-        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> queue = new ArrayDeque<>();
         queue.add(source);
         visited[source] = true;
 
