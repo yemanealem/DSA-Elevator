edbe720852da8d445d45c810a9f7cffaefe310f6
yemanealem <yemanea223@gmail.com>
2026-02-26 22:30:05 +0100

    simplify path

---
+++ stack/SimplifyPath.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String simplifyPath(String path) {
+    Stack<String> stack = new Stack<>();
+        String[] parts = path.split("/");
+
+        for (String part : parts) {
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            } else if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(part);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String dir : stack) {
+            result.append("/").append(dir);
+        }
+
+        return result.length() == 0 ? "/" : result.toString()   ; 
+    }
+}
