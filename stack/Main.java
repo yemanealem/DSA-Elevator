71e38c1eea879449dc134b9dcbbbe32abed764bd
yemanealem <yemanea223@gmail.com>
2026-03-01 17:33:28 +0100

    refactored reverse linkdin list

---
+++ stack/Main.java
@@ -0,0 +1,53 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next; // store next
+            current.next = prev;              // reverse link
+            prev = current;                   // move prev
+            current = nextNode;                // move forward
+        }
+
+        return prev; // new head
+    }
+}
+
+public class Main {
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        // Create linked list: 1 -> 2 -> 3 -> 4 -> null
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        // Reverse
+        Solution solution = new Solution();
+        ListNode reversed = solution.reverseList(head);
+
+        System.out.println("Reversed List:");
+        printList(reversed);
+    }
+}
