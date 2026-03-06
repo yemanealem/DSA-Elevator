573c5a073ddec331280d9aec95d2455369cd8e00
yemanealem <yemanea223@gmail.com>
2026-03-06 18:29:38 +0100

    refactored group anagrame

--- hashtable/GroupAnagrams.java
+++ hashtable/GroupAnagrams.java
@@ -1,22 +1,42 @@
-public class GroupAnagrams {
-    
-    import java.util.*;
+// Question: Group Anagrams
+// How it works:
+// This program groups words that are anagrams of each other.
+// An anagram means words that contain same characters but in different order.
+// Example: "eat", "tea", "ate" are anagrams.
+//
+// Logic:
+// For each string, we sort its characters.
+// Sorted version becomes the key in HashMap.
+// All anagrams will have same sorted key and will be grouped together.
+//
+// Time Complexity: O(n * k log k) where n = number of strings, k = length of string.
 
-class Solution {
+import java.util.*;
+
+public class GroupAnagrams {
+
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
 
         for (String s : strs) {
             char[] chars = s.toCharArray();
             Arrays.sort(chars);
-            String key = new String(chars);
 
-            map.putIfAbsent(key, new ArrayList<>());
-            map.get(key).add(s);
+            String key = String.valueOf(chars);
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
 
         return new ArrayList<>(map.values());
     }
-}
 
+    public static void main(String[] args) {
+        GroupAnagrams ga = new GroupAnagrams();
+
+        String[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        List<List<String>> result = ga.groupAnagrams(input);
+
+        System.out.println(result);
+    }
 }
