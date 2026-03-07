234d40d71e569f2093f8382c222e7b1b4c4e6954
yemanealem <yemanea223@gmail.com>
2026-03-07 18:51:36 +0100

    refactored return the total number of continuous subarrays whose sum equals k

--- prifixsum/SubarraySum.java
+++ prifixsum/SubarraySum.java
@@ -1,11 +1,41 @@
+/*
+ * QUESTION:
+ * Subarray Sum Equals K
+ * ---------------------------
+ * Given an integer array nums and an integer k,
+ * return the total number of continuous subarrays
+ * whose sum equals k.
+ *
+ * HOW IT WORKS (Prefix Sum + HashMap):
+ * ---------------------------
+ * Let prefixSum[i] = sum of elements from index 0 to i.
+ *
+ * Sum of subarray (j+1 ... i) = prefixSum[i] - prefixSum[j]
+ *
+ * We need:
+ * prefixSum[j] = prefixSum[i] - k
+ *
+ * If prefixSum[i] - k exists in the map, that means
+ * a subarray ending at i has sum = k.
+ *
+ * We store prefix sum frequencies in a HashMap:
+ * map[prefixSum] = how many times this sum appeared.
+ *
+ * RUNNING TIME:
+ * ---------------------------
+ * - Time Complexity: O(n)
+ * - Space Complexity: O(n)
+ *
+ * This is optimal for this problem.
+ */
+
 import java.util.HashMap;
 
 public class SubarraySum {
 
     public int subarraySum(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-
-        map.put(0, 1); // Base case: prefix sum 0 exists once
+        map.put(0, 1); // base case: empty prefix sum
 
         int prefixSum = 0;
         int count = 0;
@@ -13,12 +43,11 @@ public class SubarraySum {
         for (int num : nums) {
             prefixSum += num;
 
-            // If prefixSum - k exists, add its frequency
-            if (map.containsKey(prefixSum - k)) {
-                count += map.get(prefixSum - k);
+            Integer freq = map.get(prefixSum - k);
+            if (freq != null) {
+                count += freq;
             }
 
-            // Store/update prefix sum frequency
             map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
 
