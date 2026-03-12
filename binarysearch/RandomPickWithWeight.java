2aad02ba1929c628126afa0859fce52bcbf5e0e3
yemanealem <yemanea223@gmail.com>
2026-03-12 18:41:44 +0100

    refactor random pick with weight

--- binarysearch/RandomPickWithWeight.java
+++ binarysearch/RandomPickWithWeight.java
@@ -1,10 +1,11 @@
-import java.util.Random;
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
-class RandomPickWithWeight {
+public class RandomPickWithWeight {
     private int[] prefixSum;
     private int totalSum;
-    private Random rand;
 
+    // Constructor: builds prefix sum array
     public RandomPickWithWeight(int[] w) {
         int n = w.length;
         prefixSum = new int[n];
@@ -13,34 +14,34 @@ class RandomPickWithWeight {
             prefixSum[i] = prefixSum[i - 1] + w[i];
         }
         totalSum = prefixSum[n - 1];
-        rand = new Random();
     }
 
+    // Pick an index randomly with probability proportional to weight
     public int pickIndex() {
-        // Random number in range [1, totalSum]
-        int target = rand.nextInt(totalSum) + 1;
-
-        // Binary search to find first prefixSum >= target
-        int low = 0, high = prefixSum.length - 1;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (prefixSum[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid;
-            }
+        int target = ThreadLocalRandom.current().nextInt(totalSum) + 1;
+        int idx = Arrays.binarySearch(prefixSum, target);
+        if (idx < 0) {
+            idx = - (idx + 1);
         }
-        return low;
+        return idx;
     }
 
-    // Main for testing
+    // Main method for testing
     public static void main(String[] args) {
         int[] w = {1, 3, 2};
         RandomPickWithWeight picker = new RandomPickWithWeight(w);
 
-        // Run 10 picks
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Picked index: " + picker.pickIndex());
+        // Run 20 picks to see the distribution
+        int[] count = new int[w.length];
+        for (int i = 0; i < 20; i++) {
+            int idx = picker.pickIndex();
+            count[idx]++;
+            System.out.println("Picked index: " + idx);
+        }
+
+        System.out.println("\nDistribution:");
+        for (int i = 0; i < count.length; i++) {
+            System.out.println("Index " + i + ": " + count[i] + " picks");
         }
     }
 }
