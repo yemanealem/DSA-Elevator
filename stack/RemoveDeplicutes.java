20444dcab48fdb9462e705a1154bdacf461a387e
yemanealem <yemanea223@gmail.com>
2026-03-13 18:22:39 +0100

    Remove All Adjacent Duplicates In String

---
+++ stack/RemoveDeplicutes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String removeDuplicates(String s) {
+         Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+
+            if(!stack.isEmpty() && stack.peek() == c) {
+                stack.pop(); 
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for(char c : stack) {
+            result.append(c);
+        }
+
+        return result.toString();
+    }
+}
