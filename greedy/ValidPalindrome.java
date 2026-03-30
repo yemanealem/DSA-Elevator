05a29bb384c06aa40c0129f8cd3ea20fa238e1f5
yemanealem <yemanea223@gmail.com>
2026-03-30 19:59:35 +0200

    valid palindrom

--- greedy/ValidPalindrome.java
+++ greedy/ValidPalindrome.java
@@ -1,27 +1,17 @@
 public class ValidPalindrome {
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        String s1 = "abca";
-        String s2 = "racecar";
-        String s3 = "abc";
-
-        System.out.println(solution.validPalindrome(s1)); // true
-        System.out.println(solution.validPalindrome(s2)); // true
-        System.out.println(solution.validPalindrome(s3)); // false
+        System.out.println(validPalindrome("abca"));     // true
+        System.out.println(validPalindrome("racecar"));  // true
+        System.out.println(validPalindrome("abc"));      // false
     }
-}
 
-class Solution {
-
-    public boolean validPalindrome(String s) {
+    public static boolean validPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
 
         while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
-                // Try skipping either left or right
                 return isPalindrome(s, left + 1, right) ||
                        isPalindrome(s, left, right - 1);
             }
@@ -32,7 +22,7 @@ class Solution {
         return true;
     }
 
-    private boolean isPalindrome(String s, int left, int right) {
+    private static boolean isPalindrome(String s, int left, int right) {
         while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
                 return false;
