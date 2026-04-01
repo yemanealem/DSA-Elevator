bd4637312183a3180b114a2f5d1343072095d8aa
yemanealem <yemanea223@gmail.com>
2026-04-01 18:11:31 +0200

    word break

--- hashtable/WordBreak.java
+++ hashtable/WordBreak.java
@@ -3,19 +3,25 @@ import java.util.*;
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> set = new HashSet<>(wordDict);
-        boolean[] dp = new boolean[s.length() + 1];
         
+        // Find max word length to limit checks
+        int maxLen = 0;
+        for (String word : wordDict) {
+            maxLen = Math.max(maxLen, word.length());
+        }
+
+        boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
 
         for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = i - 1; j >= 0 && (i - j) <= maxLen; j--) {
                 if (dp[j] && set.contains(s.substring(j, i))) {
                     dp[i] = true;
-                    break; // important optimization
+                    break;
                 }
             }
         }
-        
+
         return dp[s.length()];
     }
 }
