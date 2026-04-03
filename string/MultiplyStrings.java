f30191f1a97b467c7176f6d840bfe8f5aac3181a
yemanealem <yemanea223@gmail.com>
2026-04-03 12:40:04 +0200

    multiply string

--- string/MultiplyStrings.java
+++ string/MultiplyStrings.java
@@ -5,37 +5,36 @@ public class MultiplyStrings {
 
         int n = num1.length();
         int m = num2.length();
-
         int[] result = new int[n + m];
 
-        // Multiply digits
         for (int i = n - 1; i >= 0; i--) {
+            int d1 = num1.charAt(i) - '0';
+
             for (int j = m - 1; j >= 0; j--) {
-                int digit1 = num1.charAt(i) - '0';
-                int digit2 = num2.charAt(j) - '0';
+                int d2 = num2.charAt(j) - '0';
 
-                int sum = digit1 * digit2 + result[i + j + 1];
+                int pos = i + j + 1;
+                int sum = d1 * d2 + result[pos];
 
-                result[i + j + 1] = sum % 10;       // current digit
-                result[i + j] += sum / 10;          // carry
+                result[pos] = sum % 10;
+                result[pos - 1] += sum / 10;
             }
         }
 
-        // Convert to string
-        StringBuilder sb = new StringBuilder();
-        for (int num : result) {
-            if (!(sb.length() == 0 && num == 0)) {
-                sb.append(num);
-            }
+        // Build result string efficiently
+        StringBuilder sb = new StringBuilder(n + m);
+
+        int i = 0;
+        while (i < result.length && result[i] == 0) i++;
+
+        for (; i < result.length; i++) {
+            sb.append(result[i]);
         }
 
         return sb.toString();
     }
 
     public static void main(String[] args) {
-        String num1 = "123";
-        String num2 = "456";
-
-        System.out.println("Result: " + multiply(num1, num2)); // 56088
+        System.out.println(multiply("163", "95")); // 15485
     }
 }
