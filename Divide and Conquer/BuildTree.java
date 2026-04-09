e29584725b69e98f852d166a1ef542057ea8791d
yemanealem <yemanea223@gmail.com>
2026-04-09 18:10:55 +0200

    Refactrored build tree

--- Divide and Conquer/BuildTree.java
+++ Divide and Conquer/BuildTree.java
@@ -90,5 +90,19 @@ public class BuildTree {
         TreeNode root = bt.buildTree(preorder, inorder);
 
         System.out.println("Tree constructed successfully!");
+
+        /*
+        
+        preorder = [3, 9, 20, 15, 7]
+inorder  = [9, 3, 15, 20, 7]
+
+Tree:
+        3
+       / \
+      9  20
+         / \
+        15  7
+        
+        */
     }
 }
