ad5f9819b74d0f76e721b190e4120b1d4b8a8a39
yemanealem <yemanea223@gmail.com>
2026-04-11 12:53:14 +0200

    unique binary search

---
+++ binarysearch/UniqueBST.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int nodes = 2; nodes <= n; nodes++) {
+            for (int root = 1; root <= nodes; root++) {
+                dp[nodes] += dp[root - 1] * dp[nodes - root];
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n = 3;
+        System.out.println("Number of unique BSTs: " + sol.numTrees(n));
+    }
+}
