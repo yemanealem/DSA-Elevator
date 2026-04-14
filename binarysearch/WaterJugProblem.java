f51d017cb2d6d19bc3dc5ddee70fa89acbb6b9e9
yemanealem <yemanea223@gmail.com>
2026-04-14 19:16:18 +0200

    water and jug

---
+++ binarysearch/WaterJugProblem.java
@@ -0,0 +1,23 @@
+public class WaterJugProblem {
+
+    public static boolean canMeasureWater(int x, int y, int z) {
+        if (z == 0) return true;
+        if (x + y < z) return false;
+
+        return z % gcd(x, y) == 0;
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int x = 3, y = 5, z = 4;
+        System.out.println(canMeasureWater(x, y, z)); // true
+    }
+}
