e85a90719ee858a78ec886736e0978e31094f2d1
yemanealem <yemanea223@gmail.com>
2026-04-19 17:09:30 +0200

    Max Points on a Line

--- hashtable/MaxPointsOnLine.java
+++ hashtable/MaxPointsOnLine.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class MaxPointsOnLine {
 
-    // Helper to calculate GCD
     private int gcd(int a, int b) {
         if (b == 0) return a;
         return gcd(b, a % b);
