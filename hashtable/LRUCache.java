c877c4d3348d19daa2e6b74e9e381c97e3b0ea5d
yemanealem <yemanea223@gmail.com>
2026-04-19 17:13:29 +0200

    Least Recently Used (LRU)

--- hashtable/LRUCache.java
+++ hashtable/LRUCache.java
@@ -83,9 +83,9 @@ public class LRUCache {
         cache.put(3, 3); // evicts key 2
         System.out.println(cache.get(2)); // -1
 
-        cache.put(4, 4); // evicts key 1
-        System.out.println(cache.get(1)); // -1
-        System.out.println(cache.get(3)); // 3
-        System.out.println(cache.get(4)); // 4
+        cache.put(4, 4); 
+        System.out.println(cache.get(1)); 
+        System.out.println(cache.get(3));
+        System.out.println(cache.get(4)); 
     }
 }
