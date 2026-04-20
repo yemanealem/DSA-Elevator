da6668e27e9572ebe02e41be09a908e7dfa76cef
yemanealem <yemanea223@gmail.com>
2026-04-20 18:24:20 +0200

    Sort Characters By Frequency

---
+++ heap/FrequencySortBucket.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class FrequencySortBucket {
+
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        // Count frequency
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        // Bucket array
+        List<Character>[] buckets = new List[s.length() + 1];
+
+        for (char c : map.keySet()) {
+            int freq = map.get(c);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(c);
+        }
+
+        // Build result
+        StringBuilder result = new StringBuilder();
+
+        for (int i = buckets.length - 1; i >= 0; i--) {
+            if (buckets[i] != null) {
+                for (char c : buckets[i]) {
+                    for (int j = 0; j < i; j++) {
+                        result.append(c);
+                    }
+                }
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        FrequencySortBucket obj = new FrequencySortBucket();
+
+        String input = "tree";
+        String output = obj.frequencySort(input);
+
+        System.out.println("Input: " + input);
+        System.out.println("Output: " + output);
+    }
+}
