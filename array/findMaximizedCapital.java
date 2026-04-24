fadd49df2bef6d679cee9c3134b0163726cc03b6
yemanealem <yemanea223@gmail.com>
2026-04-24 16:05:07 +0200

    IPO problem

--- array/findMaximizedCapital.java
+++ array/findMaximizedCapital.java
@@ -13,26 +13,21 @@ class Solution {
             projects[i][1] = profits[i];
         }
 
-        // Sort by capital
         Arrays.sort(projects, (a, b) -> a[0] - b[0]);
 
-        // Max heap for profit
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
         int i = 0;
 
         for (int j = 0; j < k; j++) {
 
-            // add all affordable projects
             while (i < n && projects[i][0] <= w) {
                 pq.add(projects[i][1]);
                 i++;
             }
 
-            // if no project available
             if (pq.isEmpty()) break;
 
-            // pick best profit
             w += pq.poll();
         }
 
