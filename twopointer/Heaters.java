1b00eb1268e3ab7b61e3a1cca10a8de3be3ee650
yemanealem <yemanea223@gmail.com>
2026-04-28 15:32:19 +0200

    Heaters

---
+++ twopointer/Heaters.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Heaters {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(heaters);
+        int radius = 0;
+
+        for (int house : houses) {
+            int index = Arrays.binarySearch(heaters, house);
+
+            if (index < 0) {
+                index = -(index + 1);
+            }
+
+            int dist1 = (index - 1 >= 0) ? house - heaters[index - 1] : Integer.MAX_VALUE;
+            int dist2 = (index < heaters.length) ? heaters[index] - house : Integer.MAX_VALUE;
+
+            int closest = Math.min(dist1, dist2);
+            radius = Math.max(radius, closest);
+        }
+
+        return radius;
+    }
+}
