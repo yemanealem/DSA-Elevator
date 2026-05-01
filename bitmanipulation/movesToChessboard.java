c194019794922e96034383c9e59c853018b5a518
yemanealem <yemanea223@gmail.com>
2026-05-01 17:16:41 +0200

    Transform to Chessboard

--- bitmanipulation/movesToChessboard.java
+++ bitmanipulation/movesToChessboard.java
@@ -2,77 +2,36 @@ class Solution {
     public int movesToChessboard(int[][] board) {
         int n = board.length;
 
-        int rowMask = 0, colMask = 0;
-
-        // Build bitmasks from first row and first column
         for (int i = 0; i < n; i++) {
-            rowMask |= (board[0][i] << i);
-            colMask |= (board[i][0] << i);
-        }
-
-        int reverseRowMask = ((1 << n) - 1) ^ rowMask;
-        int reverseColMask = ((1 << n) - 1) ^ colMask;
-
-        int rowCount = 0, colCount = 0;
-
-        for (int i = 0; i < n; i++) {
-            int r = 0, c = 0;
-
             for (int j = 0; j < n; j++) {
-                r |= (board[i][j] << j);
-                c |= (board[j][i] << j);
+                if ((board[0][0] ^ board[i][0] ^ board[0][j] ^ board[i][j]) != 0) {
+                    return -1;
+                }
             }
-
-            if (r != rowMask && r != reverseRowMask) return -1;
-            if (c != colMask && c != reverseColMask) return -1;
-
-            if (r == rowMask) rowCount++;
-            if (c == colMask) colCount++;
         }
 
-        int rowSwaps = getSwaps(rowMask, rowCount, n);
-        int colSwaps = getSwaps(colMask, colCount, n);
-
-        if (rowSwaps == -1 || colSwaps == -1) return -1;
-
-        return rowSwaps + colSwaps;
-    }
-
-    private int getSwaps(int mask, int count, int n) {
-
-        int ones = Integer.bitCount(mask);
-
-        // invalid distribution check
-        if (n % 2 == 0) {
-            if (ones != n / 2) return -1;
-        } else {
-            if (Math.abs(n - 2 * ones) != 1) return -1;
-        }
-
-        int pattern1 = 0; // 0101...
-        int pattern2 = 0; // 1010...
+        int rowSum = 0, colSum = 0;
+        int rowSwap = 0, colSwap = 0;
 
         for (int i = 0; i < n; i++) {
-            pattern1 |= (i % 2) << i;
-            pattern2 |= ((i + 1) % 2) << i;
+            rowSum += board[0][i];
+            colSum += board[i][0];
+
+            if (board[i][0] == i % 2) rowSwap++;
+            if (board[0][i] == i % 2) colSwap++;
         }
 
-        int swaps1 = Integer.bitCount(mask ^ pattern1);
-        int swaps2 = Integer.bitCount(mask ^ pattern2);
-
-        int res = Integer.MAX_VALUE;
+        if (rowSum < n / 2 || rowSum > (n + 1) / 2) return -1;
+        if (colSum < n / 2 || colSum > (n + 1) / 2) return -1;
 
         if (n % 2 == 0) {
-            res = Math.min(swaps1, swaps2);
+            rowSwap = Math.min(rowSwap, n - rowSwap);
+            colSwap = Math.min(colSwap, n - colSwap);
         } else {
-            // choose correct parity pattern
-            if (ones * 2 > n) {
-                res = swaps1;
-            } else {
-                res = swaps2;
-            }
+            if (rowSwap % 2 == 1) rowSwap = n - rowSwap;
+            if (colSwap % 2 == 1) colSwap = n - colSwap;
         }
 
-        return res / 2;
+        return (rowSwap + colSwap) / 2;
     }
 }
