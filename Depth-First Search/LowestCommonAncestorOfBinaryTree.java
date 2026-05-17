2100bf1bd65380e595ec7b7c0c81d086809e5845
yemanealem <yemanea223@gmail.com>
2026-05-17 23:46:54 +0200

    lowest-common-ancestor-of-a-binary-tree

---
+++ Depth-First Search/LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,65 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class LowestCommonAncestorOfBinaryTree {
+
+    // Depth First Search (DFS)
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        // Base case
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        // Search left subtree
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+
+        // Search right subtree
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        // If both sides return non-null,
+        // current node is the Lowest Common Ancestor
+        if (left != null && right != null) {
+            return root;
+        }
+
+        // Otherwise return the non-null side
+        return left != null ? left : right;
+    }
+
+    public static void main(String[] args) {
+
+        /*
+                 3
+                / \
+               5   1
+              / \ / \
+             6  2 0  8
+        */
+
+        TreeNode root = new TreeNode(3);
+
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+
+        TreeNode p = root.left;   // Node 5
+        TreeNode q = root.right;  // Node 1
+
+        TreeNode lca = lowestCommonAncestor(root, p, q);
+
+        System.out.println("Lowest Common Ancestor: " + lca.val);
+    }
+}
